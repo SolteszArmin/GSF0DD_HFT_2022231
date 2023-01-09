428a38e7fa9dd9fdaee517609fefc2e6d6bbad3e
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 14:43:58 +0100

    Removed more unnecery usings

--- GSF0DD_HFT_2022231.Repository/Data/GameListDbContext.cs
+++ GSF0DD_HFT_2022231.Repository/Data/GameListDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using GSF0DD_HFT_2022231.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GSF0DD_HFT_2022231.Repository.Data
 {
