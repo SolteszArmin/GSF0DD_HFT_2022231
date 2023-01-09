1245a53f429fad15e53b1defdde06d5d04e61777
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 12:38:56 +0100

    Logic main classes created

---
+++ GSF0DD_HFT_2022231.Logic/Classes/GameLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSF0DD_HFT_2022231.Logic.Classes
+{
+    internal class GameLogic
+    {
+    }
+}
