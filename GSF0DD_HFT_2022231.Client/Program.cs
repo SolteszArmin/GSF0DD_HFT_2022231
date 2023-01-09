acbaebce5d0cbe0b5e6e14ed48e0d6081510403b
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 12:10:00 +0100

    Add project files.

---
+++ GSF0DD_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GSF0DD_HFT_2022231.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
