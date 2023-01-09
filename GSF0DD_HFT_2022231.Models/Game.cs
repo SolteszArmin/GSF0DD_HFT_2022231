20a8e158785cf047a00f9f6c6db3a6b4d0156f17
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 14:43:26 +0100

    Removed unnecesery usings

--- GSF0DD_HFT_2022231.Models/Game.cs
+++ GSF0DD_HFT_2022231.Models/Game.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace GSF0DD_HFT_2022231.Models
 {
