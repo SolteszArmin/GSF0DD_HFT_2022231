355b6dcb685546066e950d94306fe4f7d536abe3
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 12:16:09 +0100

    Base Projects created with dependencies

---
+++ GSF0DD_HFT_2022231.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GSF0DD_HFT_2022231.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ GSF0DD_HFT_2022231.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GSF0DD_HFT_2022231.Test
+{
+    public class Class1
+    {
+    }
+}
