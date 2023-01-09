b53d0593d14589c784e46c192eb5e55b58570b63
solteszarmin@stud.uni-obuda.hu <solteszarmin@stud.uni-obuda.hu>
2023-01-09 12:28:00 +0100

    repository base classes created

--- GSF0DD_HFT_2022231.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GSF0DD_HFT_2022231.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ GSF0DD_HFT_2022231.Repository/Repositories/GenreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSF0DD_HFT_2022231.Repository.Repositories
+{
+    internal class GenreRepository
+    {
+    }
+}
